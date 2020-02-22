38f74d28db782f3cbc37602dbd003d76b3967470
mark sheekey <marksheekey@hotmail.com>
2020-02-22 13:28:28 +0000

    komoot part one

--- app/src/main/java/co/uk/happyapper/trailorganiser/mainscreen/MainFragmentViewModel.kt
+++ app/src/main/java/co/uk/happyapper/trailorganiser/mainscreen/MainFragmentViewModel.kt
@@ -5,7 +5,8 @@ import co.uk.happyapper.trailorganiser.global.LocalDataInterface
 
 class MainFragmentViewModel(val localData: LocalDataInterface) :
     ViewModel() {
-    var  komootUserToken = localData.komootUserToken
+    //var  komootUserToken = localData.komootUserToken
+    var komootUserToken = "hello"
 }
 
 
